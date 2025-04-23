ca3e58fe57576701f451023103a75093e9890463
jin123457 <jinsol9909@gmail.com>
2025-04-24 00:47:00 +0900

    ✅ 4월 24일 구현 2번 문제 해결

---
+++ 2025.04/1주차/04.24/2번 문제/11650.js
@@ -0,0 +1,20 @@
+// const readLine = require('fs').readFileSync('dev/stdin').toString().trim();
+const readLine = `5
+3 4
+1 1
+1 -1
+2 2
+3 3`;
+const [N, ...input] = readLine.split('\n');
+
+const answer = input.sort((a, b) => {
+  const [x1, y1] = a.split(' ').map(Number);
+  const [x2, y2] = b.split(' ').map(Number);
+
+  if (x1 === x2) {
+    return y1 - y2;
+  }
+  return x1 - x2;
+});
+
+console.log(answer.join('\n'));
