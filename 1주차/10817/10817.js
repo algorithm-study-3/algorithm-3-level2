c65ea439ace8bfe3c0c0d56f58f1d66c9cc5ed0f
hoyyChoi <hoyeon8165@gmail.com>
2025-04-28 20:47:43 +0900

    feat: 10817

---
+++ 1주차/10817/10817.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+// TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+// 풀이
+function solution(arr) {
+  const sortArr = arr[0].split(" ").sort((a, b) => b - a);
+  console.log(sortArr[1]);
+}
+
+solution(input);
