67a9d8b68338f8810f120f207f543ba3a551e84a
jin123457 <jinsol9909@gmail.com>
2025-05-20 14:37:39 +0900

    ✅ 5월 20일 1번 문제 수학 문제 해결

---
+++ 2025.05/5주차/05.20/1번 문제/1676.js
@@ -0,0 +1,10 @@
+// const readLine = require('fs').readFileSync('dev/stdin').toString().trim();
+const readLine = `10`;
+let N = Number(readLine);
+
+let count = 0;
+while (N >= 5) {
+  N = Math.floor(N / 5);
+  count += N;
+}
+console.log(count);
