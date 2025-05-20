a0e0867f4f580f725bdf7a16320a4aab323e23ab
jin123457 <jinsol9909@gmail.com>
2025-05-20 21:55:18 +0900

    ✅ 5월 20일 큐 문제 해결

---
+++ 2025.05/5주차/05.20/1966.js
@@ -0,0 +1,49 @@
+//const readLine = require('fs').readFileSync('dev/stdin').toString().trim();
+const readLine = `3
+1 0
+5
+4 2
+1 2 3 4
+6 0
+1 1 9 1 1 1`;
+const [N, ...input] = readLine.split('\n');
+const answer = [];
+
+for (let i = 0; i < input.length; i += 2) {
+  const [docsCount, currentDocs] = input[i].split(' ').map((number) => Number(number));
+  const docsQueue = input[i + 1].split(' ').map((number) => Number(number));
+
+  const map = new Map();
+  const levelMap = new Map();
+  docsQueue.forEach((docs, i) => {
+    map.set(i, docs);
+    levelMap.set(docs, levelMap.has(docs) ? levelMap.get(docs) + 1 : 1);
+  });
+  console.log(map, levelMap);
+  const sortLevelMap = [...levelMap].sort((a, b) => b[0] - a[0]);
+  const newMap = [...map];
+  let num1 = 0;
+  let find = false;
+  while ([...map].length > 0) {
+    if (sortLevelMap[0][0] > newMap[0][1]) {
+      newMap.push(newMap.shift());
+    } else {
+      if (sortLevelMap[0][1] > 1) {
+        sortLevelMap[0][1] -= 1;
+      } else {
+        sortLevelMap.shift();
+      }
+
+      const [index, num] = newMap.shift();
+
+      num1 += 1;
+      if (index === currentDocs) {
+        find = true;
+        break;
+      }
+    }
+
+    if (find) break;
+  }
+  console.log(num1);
+}
