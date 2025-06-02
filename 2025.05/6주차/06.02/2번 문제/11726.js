588672621842673932531913d272d554c89b05e3
jin123457 <jinsol9909@gmail.com>
2025-06-02 17:41:46 +0900

    ✅ 6월 2일 2번 문제 다이나믹 프로그래밍 문제 해결

---
+++ 2025.05/6주차/06.02/2번 문제/11726.js
@@ -0,0 +1,11 @@
+//const readLine = require('fs').readFileSync('dev/stdin').toString().trim();
+const readLine = `1000`;
+const N = Number(readLine);
+
+const answer = [0, 1, 2];
+
+for (let i = 3; i <= N; i++) {
+  answer.push((answer[i - 1] + answer[i - 2]) % 10007);
+}
+
+console.log(answer[N]);
